620bc251fe5c7356bcbe44eefbedbe63cd536fe5
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-01-25 14:04:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-resource "azurerm_resource_group" "bnrrg" {
+resource "azurerm_resource_group" "bnryrg" {
    name = var.rgname
    location = var.loc
   
 }
 
-resource "azurerm_public_ip" "bnrrg" {
+resource "azurerm_public_ip" "bnryrg" {
     name = var.pipname
-    resource_group_name = azurerm_resource_group.bnrrg.name
-    location = azurerm_resource_group.bnrrg.location
+    resource_group_name = azurerm_resource_group.bnryrg.name
+    location = azurerm_resource_group.bnryrg.location
     allocation_method   = "Static"
 
   tags = {
     environment = "Production"
   }
+}
